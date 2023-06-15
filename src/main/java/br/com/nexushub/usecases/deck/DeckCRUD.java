32817b7a2f0d7c7c2a204da7e225836cb27b7351
Arthur Mascaro <arthumascaro69@gmail.com>
2023-06-15 09:56:50 -0300

    Feat: Deck CRUD and DAO interfaces methods

--- src/main/java/br/com/nexushub/usecases/deck/DeckCRUD.java
+++ src/main/java/br/com/nexushub/usecases/deck/DeckCRUD.java
@@ -1,4 +1,21 @@
 package br.com.nexushub.usecases.deck;
 
+import br.com.nexushub.domain.Deck;
+import br.com.nexushub.web.model.deck.request.DeckRequest;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface DeckCRUD {
+
+    Deck createNewDeck(DeckRequest deckRequest);
+
+    Deck findDeckById(UUID id);
+
+    Deck updateDeckById(UUID id, DeckRequest deckRequest);
+
+    List<Deck> findAllDeckByUserId();
+
+    Deck deleteDeckById(UUID id);
+
 }
--- src/main/java/br/com/nexushub/usecases/deck/gateway/DeckDAO.java
+++ src/main/java/br/com/nexushub/usecases/deck/gateway/DeckDAO.java
@@ -2,7 +2,19 @@ package br.com.nexushub.usecases.deck.gateway;
 
 import br.com.nexushub.domain.Deck;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DeckDAO{
+
+    Deck saveNewDeck(Deck deck);
+
+    Deck updateDeck(Deck deck);
+
+    Optional<Deck> findDeckById(UUID id);
+
+    List<Deck> findAllDecksByUserId(UUID userId);
+
+    Deck deleteDeckById(UUID id);
 }
