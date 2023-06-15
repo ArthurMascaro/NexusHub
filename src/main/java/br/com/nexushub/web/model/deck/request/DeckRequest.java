1c78209a5d0e87af24ee5d8fc69734813e001e51
Arthur Mascaro <arthumascaro69@gmail.com>
2023-06-15 09:57:21 -0300

    Feat: create toDeck method

--- src/main/java/br/com/nexushub/web/model/deck/request/DeckRequest.java
+++ src/main/java/br/com/nexushub/web/model/deck/request/DeckRequest.java
@@ -1,12 +1,21 @@
 package br.com.nexushub.web.model.deck.request;
 
+import br.com.nexushub.domain.Deck;
+
+import java.util.Objects;
 import java.util.UUID;
 
 public record DeckRequest(String name, UUID subjectId, UUID parentDeckId){
 
     public DeckRequest(String name, UUID subjectId, UUID parentDeckId) {
-        this.name = name;
+        this.name = Objects.requireNonNull(name, "Name must not be null!");
+        if (name.isEmpty()) throw new IllegalArgumentException("Name must not be empty!");
+
         this.subjectId = subjectId;
         this.parentDeckId = parentDeckId;
+    }
+
+    public Deck toDeck(){
+        return Deck.createWithoutIdAndOwner(name, subjectId, parentDeckId);
     }
 }
