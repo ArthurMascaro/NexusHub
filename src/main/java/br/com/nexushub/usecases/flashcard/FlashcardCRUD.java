2411b9c5ec202e04023ffae5eb2c88336ae2e075
Arthur Mascaro <arthumascaro69@gmail.com>
2023-08-03 09:08:42 -0300

    feat: flashcard CRUD implementation

--- src/main/java/br/com/nexushub/usecases/flashcard/FlashcardCRUD.java
+++ src/main/java/br/com/nexushub/usecases/flashcard/FlashcardCRUD.java
@@ -1,4 +1,20 @@
 package br.com.nexushub.usecases.flashcard;
 
+import br.com.nexushub.domain.Flashcard;
+import br.com.nexushub.web.model.flashcard.request.FlashcardRequest;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface FlashcardCRUD {
+
+    Flashcard createNewFlashcard(FlashcardRequest flashcardRequest);
+
+    Flashcard findFlashcardById(UUID id);
+
+    Flashcard updateFlashcardById(UUID id, FlashcardRequest flashcardRequest);
+
+    Flashcard deleteFlashcardById(UUID id);
+
+    List<Flashcard> findAllFlashcardByDeckId(UUID deckId);
 }
--- src/main/java/br/com/nexushub/usecases/flashcard/FlashcardCRUDimpl.java
+++ src/main/java/br/com/nexushub/usecases/flashcard/FlashcardCRUDimpl.java
@@ -1,4 +1,64 @@
 package br.com.nexushub.usecases.flashcard;
 
+import br.com.nexushub.configuration.auth.jwt.IAuthenticationFacade;
+import br.com.nexushub.domain.Flashcard;
+import br.com.nexushub.usecases.deck.DeckCRUD;
+import br.com.nexushub.usecases.flashcard.gateway.FlashcardDAO;
+import br.com.nexushub.web.exception.ResourceNotFoundException;
+import br.com.nexushub.web.model.flashcard.request.FlashcardRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class FlashcardCRUDimpl implements FlashcardCRUD {
+
+    private final FlashcardDAO flashcardDAO;
+
+    private final DeckCRUD deckCRUD;
+
+    public FlashcardCRUDimpl(FlashcardDAO flashcardDAO, DeckCRUD deckCRUD) {
+        this.flashcardDAO = flashcardDAO;
+        this.deckCRUD = deckCRUD;
+    }
+
+    @Override
+    public Flashcard createNewFlashcard(FlashcardRequest flashcardRequest) {
+        Flashcard flashcard = flashcardRequest.toFlashcard();
+        return flashcardDAO.saveNewFlashcard(flashcard);
+    }
+
+    @Override
+    public Flashcard findFlashcardById(UUID id) {
+        return flashcardDAO.findFlashcardById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found flashcard with id: " + id));
+    }
+
+    @Override
+    public Flashcard updateFlashcardById(UUID id, FlashcardRequest flashcardRequest) {
+        Flashcard flashcardUpdate = flashcardRequest.toFlashcard();
+
+        Flashcard flashcard = flashcardDAO.findFlashcardById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found flashcard with id: " + id));
+
+        if (flashcardUpdate.getDeckId() != null)
+            deckCRUD.findDeckById(flashcardUpdate.getDeckId());
+
+        flashcardUpdate.setId(id);
+        return flashcardDAO.updateFlashcard(flashcardUpdate);
+    }
+
+    @Override
+    public Flashcard deleteFlashcardById(UUID id) {
+        flashcardDAO.findFlashcardById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Not found flashcard with id: " + id));
+
+        return flashcardDAO.deleteFlashcardById(id);
+    }
+
+    @Override
+    public List<Flashcard> findAllFlashcardByDeckId(UUID deckId) {
+        return flashcardDAO.findAllFlashcardByDeckId(deckId);
+    }
 }
